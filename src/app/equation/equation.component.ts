3504adb25d9276b0b21977d85f8c4d89ce125802
rounakbhowmick <rounakchakda@gmail.com>
2021-11-05 18:06:16 +0530

    functionality completed

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,29 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MathValidators } from '../math-validators';
+import { delay, filter, scan } from 'rxjs/operators'
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
     answer: new FormControl('', [Validators.required]),
   }, [
-   MathValidators.addition('answer','a','b')
+    MathValidators.addition('answer', 'a', 'b')
   ]);
   constructor() { }
 
   ngOnInit(): void {
-    this.mathForm.statusChanges.subscribe(value=>{
-      if(value=='INVALID'){
-        return;
-      }
-      this.mathForm.controls.a.setValue(this.randomNumber());
-      this.mathForm.controls.b.setValue(this.randomNumber());
-      this.mathForm.controls.answer.setValue('');
+    // const startTime=new Date();
+    // let numberSolved=0;
+    this.mathForm.statusChanges.pipe(
+      filter(value => value === 'VALID'),
+      delay(500),
+      scan((acc) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, { numberSolved: 0, startTime: new Date() })
+
+    ).subscribe(({ numberSolved, startTime }) => {
+      console.log("starttime",startTime.getTime());
+      console.log("numbersolved",numberSolved)
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000
+      // if (value == 'INVALID') {
+      //   return;
+      // }
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ''
+      })
+      // this.mathForm.controls.a.setValue(this.randomNumber());
+      // this.mathForm.controls.b.setValue(this.randomNumber());
+      // this.mathForm.controls.answer.setValue('');
     })
   }
   get a() {
--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -3,9 +3,6 @@ import { AbstractControl } from "@angular/forms";
 export class MathValidators {
   static addition(target: string, sourceOne: string, sourceTwo: string) {
     return (form:AbstractControl) => {
-      console.log("form",form);
-      console.log("target",target);
-      console.log("a",sourceOne);
       const sum=form.value[target];
       const firstNumber=form.value[sourceOne];
       const secondNumber=form.value[sourceTwo]
